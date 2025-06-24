d23f2c98f8c407423f73bb51add9d6d90cf04ed0
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2025-06-24 16:50:55 +0600

    updated

--- src/helpers/serverHomePage.js
+++ src/helpers/serverHomePage.js
@@ -132,7 +132,7 @@ function serverHomePage(req, responseTimes) {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>${process.env.PROJECT_NAME} Server</title>
+        <title>${process.env.PROJECT_NAME} Server working</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
