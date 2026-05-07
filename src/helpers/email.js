e7f6e5ef212351b8655795efff4a529d40e417e8
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2026-05-07 10:05:58 +0600

    woking in progress

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -64,34 +64,91 @@
 // module.exports = emailWithNodemailer;
 
 
+// const nodemailer = require("nodemailer");
+
+// const emailWithNodemailer = async (emailData) => {
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+    // auth: {
+    //   user: 'raseldevsta@gmail.com',
+    //   pass: 'xtqk ierl blgp nbzq', // app password
+    // },
+//   });
+
+//   try {
+//     console.log('Mail send started...');
+//     await transporter.sendMail({
+//       from: 'raseldevsta@gmail.com',
+//       to: emailData.email,
+//       subject: emailData.subject,
+//       text: '', // optional
+//       html: emailData.html,
+//     });
+//     console.log("Successfully sent!");
+//     return { success: true, message: 'Email sent successfully' };
+//   } catch (error) {
+//     console.error('Send mail error:', error);
+//     return { success: false, message: 'Failed to send email', error: error.message };
+//   }
+// };
+
+// module.exports = emailWithNodemailer;
+
+
+
 const nodemailer = require("nodemailer");
 
-const emailWithNodemailer = async (emailData) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+const isProduction = process.env.NODE_ENV === 'production';
+
+// console.log(" isProduction:", isProduction);
+// console.log(" config.smtp.host:", config.smtp.host);
+// console.log(" config.smtp.user:", config.smtp.user);
+// console.log(" config.smtp.pass:", config.smtp.pass);
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com', // sending SMTP server
+  port: isProduction ? 465 : 587,             // SSL port
+  secure: isProduction,           // true for port 465
     auth: {
       user: 'raseldevsta@gmail.com',
       pass: 'xtqk ierl blgp nbzq', // app password
     },
-  });
+  tls: { rejectUnauthorized: false },
+});
 
-  try {
-    console.log('Mail send started...');
-    await transporter.sendMail({
-      from: 'raseldevsta@gmail.com',
-      to: emailData.email,
-      subject: emailData.subject,
-      text: '', // optional
-      html: emailData.html,
-    });
-    console.log("Successfully sent!");
-    return { success: true, message: 'Email sent successfully' };
-  } catch (error) {
-    console.error('Send mail error:', error);
-    return { success: false, message: 'Failed to send email', error: error.message };
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error('SMTP connection failed', err);
+  } else {
+    console.log('SMTP is ready to send mail');
   }
+});  
+
+
+const emailWithNodemailer = async (emailData) => {
+
+
+
+    try {
+     console.log('mail send started......');
+    await transporter.sendMail({
+      from: 'raseldevsta@gmail.com', // sender address
+      to: emailData.email, // list of receivers
+      subject: emailData.subject,
+      html: emailData.html, // html body
+    });
+
+    console.log('mail sended successfully.....');
+    
+  } catch (error) {
+    console.log('send mail error:', error);
+    
+  }
+  console.log('mail sended stopped');
+
 };
 
 module.exports = emailWithNodemailer;
